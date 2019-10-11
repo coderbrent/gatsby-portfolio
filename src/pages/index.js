a6c51bb010ebd403519b897a467c5435b6dccd45
Brent Abruzese <brent.abruzese@gmail.com>
2019-10-11 00:21:37 -0400

    added font-awesome

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Image from '../components/image'
-
+ 
 const IndexPage = () => (
   <Layout>
     <h3 className="heading">A little bit about me!</h3>
