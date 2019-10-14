a8642772eb5db4953a5b017a847f4a7e277dbec3
Brent Abruzese <brent.abruzese@gmail.com>
2019-10-14 16:11:27 -0400

    added leading / to path

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -2,7 +2,17 @@ import React from 'react'
 import '../card/card.css'
 
 const Card = ({ name, date, img, site, github, desc, stack }) => {
-  
+  //TODO: Conditionally render 'stack' icons based on tech used in a project
+  //... a cool feature would be to iterate over the description and search for
+  //the specific entry 
+
+  const readDesc = (proj) => {
+    const myProject = proj;
+
+    myProject.forEach()
+  }
+
+
 return (
   <>
     <div className="card flipInX">
--- src/pages/2019-09-11/index.md
+++ src/pages/2019-09-11/index.md
@@ -2,7 +2,7 @@
 path: "/post-two"
 date: "2019-10-11"
 title: "How Bollywood Movies Keep Me Going"
-author: "Som Ramnanni"
+author: "Som Ramnani"
 ---
 
 "Is this what they refer to as 'callback hell?', said Som Ramnani, recent Code BC grad and international lover of all things Bollywood. He'd just finished his 6th Bollywood film ... and it was still only Saturday.
