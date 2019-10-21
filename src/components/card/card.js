524c020590080dbc8b47febe0f2a7cb520b0196b
Brent Abruzese <brent.abruzese@gmail.com>
2019-10-21 13:18:16 -0400

    added links to card headers

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import cardStyles from '../card/card.module.css'
+import CodeOnlyIcon from '../../Icons/code.svg'
+import DeployedIcon from '../../Icons/deployed.svg'
 
 const Card = ({ name, date, img, site, github, desc, stack }) => {
   
@@ -7,13 +9,28 @@ return (
   <>
     <div className={cardStyles.card}>
       <a href={site} style={{ textDecoration: 'none '}}>
-        <div className={cardStyles.cardHeader} style={{ display: 'inline-flex'}}>
+        <div className={cardStyles.cardHeader}>
           <div className={cardStyles.cardTitle}>
             {name}
           </div>
+            <div>
+              <a href={github}
+                target="_blank"
+              >
+                <CodeOnlyIcon 
+                  className={cardStyles.cardTitleDeployLink} 
+                  />
+              </a> &nbsp;
+              <a href={site}
+                target="_blank"
+              >
+                <DeployedIcon 
+                  className={cardStyles.cardTitleCodeLink} 
+                  />
+              </a>
+            </div>
         </div>
           <div className={cardStyles.cardBody}> {desc} </div>
-          <img src={img} className={cardStyles.cardImg}/>
       </a>
     </div>
   </>
