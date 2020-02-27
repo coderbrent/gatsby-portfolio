2f891faa8a67f4c86aaf1fa41f5024cee3404a3e
Brent Abruzese <brent.abruzese@gmail.com>
2020-02-27 14:18:29 -0500

    fixed themeprovider path in 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Link } from 'gatsby'
-import ThemeProvider from '../components/ThemeProvider';
+import ThemeProvider from '../context/ThemeProvider';
 
 const NotFoundPage = () => (
   <ThemeProvider>
