0b3fdb12d350fc0e67cd0a1afeb36040eba771e2
Brent Abruzese <brent.abruzese@gmail.com>
2020-02-27 14:15:30 -0500

    added theme provider to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,14 +2,17 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { Link } from 'gatsby'
+import ThemeProvider from '../components/ThemeProvider';
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1 className="heading">Ruh roh...</h1>
-      <p>What you gon' do, when the 404 come lookin' fo you?</p>
-        <Link to="/">Head back home!</Link>
-  </Layout>
+  <ThemeProvider>
+    <Layout>
+      <SEO title="404: Not found" />
+      <h1 className="heading">Ruh roh...</h1>
+        <p>That page could not be found!</p>
+          <Link to="/">Head back home!</Link>
+    </Layout>
+  </ThemeProvider>
 )
 
 export default NotFoundPage
