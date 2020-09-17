df862c3fd185c72c26096d0b43905c48a50159a5
Brent Abruzese <brent.abruzese@gmail.com>
2020-09-17 12:47:41 -0400

    updated the ever important opening line

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,7 +59,7 @@ const IndexPage = () => {
                 textAlign: `left`,
                 transition: `0.8s cubic-bezier(0.2, 0.8, 0.2, 1)`
             }}}>
-            Hi! I build things with <span css={{ color: 'gold'}}>JavaScript</span> for people!
+            Hi! I build things with <span css={{ color: 'gold'}}>JavaScript</span> for people to use!
           </h2>
             <p 
               css={{ 
